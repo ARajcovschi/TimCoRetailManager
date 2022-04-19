2ac7c3e7c147a87c7ca59a0bad2664a9bcad59fe
Alexandr Rajcovschi <alexandr.rajcovschi@gmail.com>
2022-04-19 21:12:17 +0300

    Add Login Form
    Added first version of login form with
    Username and Password check
    Created Helper Folder with PasswordBox class

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel: Conductor<object>
     {
+        private LoginViewModel _loginWM;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginWM = loginVM;
+            ActivateItemAsync(_loginWM);
+        }
+
+
+
     }
 }
