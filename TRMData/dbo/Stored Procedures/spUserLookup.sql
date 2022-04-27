919f22c5182d304fb6d2fde44d8b5b6316b94316
Alexandr Rajcovschi <alexandr.rajcovschi@gmail.com>
2022-04-27 22:28:33 +0300

    Add Get User Data Functionality
    Created folders and classes to connect DB with API
    Added TRMDataManager.Library project
    Added stored procedures files to TRMData

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+	
+AS
+begin
+	set nocount on;
+
+	select Id, FirstName, LastName, EmailAddress
+	from [dbo].[User]
+	where Id = @id;
+end
+
+
